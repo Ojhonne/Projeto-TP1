da901653d95fbb618cba62aaf75665652255c036
Fabio-AS06 <willianfabio549@gmail.com>
2026-03-27 21:41:51 -0300

    agora vai

---
+++ headers/Dominios/senha.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+using namespace std;
+
+class Senha {
+    private:
+        static const int tam_maximo = 6;
+        string senha;
+        bool validaSenha(string);
+    public:
+        bool setSenha(string);
+        string getSenha();
+};
+
+inline string Senha::getSenha() {
+    return senha;
+}
