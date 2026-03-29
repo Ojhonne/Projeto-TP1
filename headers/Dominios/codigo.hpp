d2ba181aa8d79d82df0504c2dbd6518b2800068c
edsanki20 <edusanki123@gmail.com>
2026-03-29 15:40:59 -0300

    Add files via upload

---
+++ headers/Dominios/codigo.hpp
@@ -0,0 +1,24 @@
+#ifndef DOMINIOS_HPP_INCLUDED
+#define DOMINIOS_HPP_INCLUDED
+
+using namespace std;
+
+class Codigo {
+
+    private:
+        char codigo[6];
+        static const int TAMANHO = 5;
+        bool valida(const char s[]);
+
+    public:
+        bool setCodigo(const char* s);
+        const char* getCodigo();
+};
+
+inline const char* Codigo::getCodigo()
+{
+    return codigo;
+}
+
+
+#endif
