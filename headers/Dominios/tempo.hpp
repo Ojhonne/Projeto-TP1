d84fded172dce832be19391aa95c0bb238d8857c
João Francisco <jhonjhon2sj@gmail.com>
2026-04-12 12:27:30 -0300

    Consertando um bug

--- headers/Dominios/tempo.hpp
+++ headers/Dominios/tempo.hpp
@@ -1,19 +1,19 @@
 #ifndef TEMPO_HPP
 #define TEMPO_HPP
-
+#include <string>
 class Tempo{
 
     private:
-       string tempo; 
-        bool validar(const string&);
+        std::string tempo; 
+        bool validar(const std::string&);
         static const int TAM_MAXIMO = 365;
         static const int TAM_MINIMO = 1;
     public:
-        void setTempo(const string&);
-        int getTempo() const;
+        void setTempo(const std::string&);
+        std::string getTempo() const;
 
 };
-inline int Tempo::getTempo() const{
+inline std::string Tempo::getTempo() const{
     return tempo;
 }
 
