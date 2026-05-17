8ae31a908d4085f0dee702615a1c636329686d10
João Francisco <jhonjhon2sj@gmail.com>
2026-05-17 15:50:17 -0300

    feat:documentação nome

--- headers/Dominios/dominios.hpp
+++ headers/Dominios/dominios.hpp
@@ -1,15 +1,57 @@
+/**
+ * @file Dominio.hpp
+ * @author João Francisco 
+ * @brief Definição da classe de domínio Nome para gerenciar a validação do nome.
+ */
 #ifndef DOMINIOS_HPP_INCLUDED
 #define DOMINIOS_HPP_INCLUDED
 
 #include <string>
 #include <stdexcept>
 
-class Dominio{ //classe base Abstrata
+/**
+ * @class Dominio
+ * @brief Classe base abstrata que padroniza o comportamento de classes de domínio.
+ *
+ * A classe Dominio gerencia o armazenamento de uma string e garante que
+ * apenas valores válidos sejam atribuídos, delegando a regra de validação
+ * para as classes derivadas (filhas) através de um método virtual puro.
+ */
+
+class Dominio{ 
     private:
+        /**
+        * @brief string que represanta o valor interno armazenado após validação.
+         */
         std::string valor;
-        virtual void validar(const std::string&) = 0; // metodo que as classes filhas devem implementar
+        /**
+         * @brief Método virtual puro para validação das regras de cada Domínio.
+         * 
+         * Este método dita o contrato de validação. Ele deve ser 
+         * obrigatoriamente implementado pelas classes filhas (como a classe Nome) 
+         * contendo as regras específicas de formatação e tamanho.
+         * 
+         * @param valor A string que será submetida à validação.
+         * @throw std::invalid_argument Se o valor não atender às regras da classe filha.
+         */
+        virtual void validar(const std::string&) = 0;
     public:
+        /**
+         * @brief Define um novo valor para o domínio.
+         * 
+         * Este método recebe uma string e aciona internamente o método validar() 
+         * implementado pela classe filha. O valor só será armazenado se a 
+         * validação for bem-sucedida.
+         * 
+         * @param valor O novo valor a ser armazenado.
+         * @throw std::invalid_argument Se a validação falhar.
+         */
         virtual void setValor(const std::string&);
+        /**
+         * @brief Retorna o valor atualmente armazenado no domínio.
+         * 
+         * @return std::string O valor validado.
+         */
         std::string getValor() const;
 };
 
@@ -61,14 +103,52 @@ inline void Estado::validar(const std::string& strEstado){
 }
 
 
+/**
+ * @class Nome
+ * @brief Representa o formato válido para armazenar um nome.
+ *
+ * A classe domínio Nome herda as operações públicas da classe abstrata Dominio.
+ * O dominiío Nome representa palavras que distinguem e identificam uma coisa própria com regras formais
+ * de espaçamento e caracteres permitidos.
+ *
+ * Regras:
+ *  - Tamanho de até 10 caracteres.
+ *  - Não pode ser vazio.
+ *  - Não pode começar ou terminar com espaço.
+ *  - Espaço em branco é deve ser seguido por letra.
+ *  - Cada termo deve iniciar com letra maiúscula ou minúscula.
+ *  - Apenas letras e espaços são permitidos.
+ */
 
 class Nome : public Dominio{
     private:
         static const int TAM_MAXIMO{10};
+        /**
+         * @brief Valida o nome segundo as regras do domínio.
+         *
+         * @param valor Nome em formato string.
+         * @throw std::invalid_argument ou std::Length_error Caso alguma regra seja violada.
+         */
         void validar(const std::string&);
+        /**
+         * @brief Método auxiliar para verificar se a string é alfanumérica.
+         * @param str A string a ser verificada.
+         * @return true se todos os caracteres forem alfanuméricos, false caso contrário.
+         */
         bool ehValido(const std::string&);
+         /**
+         * @brief Método auxiliar para validar as regras de espaçamento do nome.
+         * @param str A string a ser verificada.
+         * @return true se todos os caracteres estiverem no formato valído, false caso contrário.
+         */
         bool temEspacoValido(const std::string&);
     public:
+        /**
+         * @brief Construtor padrão.
+         *
+         * Inicializa o nome com string "Sem nome".
+         * O valor só será validado quando setValor() for chamado.
+         */
         Nome();
 };
 inline Nome::Nome(){
