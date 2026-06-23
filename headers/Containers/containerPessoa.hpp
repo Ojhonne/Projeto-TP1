9bede79a59add946b1b153742b852ee3ea450bbc
joaoF <jhonjhon2sj@gmail.com>
2026-06-23 15:45:24 -0300

    feat: declaracao do container pessoa

---
+++ headers/Containers/containerPessoa.hpp
@@ -0,0 +1,70 @@
+/**
+ * @file containerPessoa.hpp
+ * @author João Francisco de Sousa Torres
+ * @brief Definição do container de pessoas.
+* @details Este container é responsável por armazenar e gerenciar os objetos de domínio
+ * relacionados às pessoas do sistema utilizando o banco de dados SQLite. Ele implementa operações 
+ * de inclusão, remoção, pesquisa e atualização de pessoas, garantindo a integridade dos dados e a 
+ * consistência das informações armazenadas.
+ */
+
+#ifndef CONTAINERPESSOA_H_INCLUDED
+#define CONTAINERPESSOA_H_INCLUDED
+
+// Forward declarations.
+class Email;
+class Pessoa;
+
+
+/**
+ * @class ContainerPessoa
+ * @brief Implementa um container para armazenar e gerenciar objetos de domínio relacionados às pessoas
+ * 
+ */
+
+//----------------------------------------------------------------------------------------
+// Declaração de classe container.
+//
+// Declaração adota padrão de projeto Singleton.
+
+
+class ContainerPessoa{
+    private:
+        static ContainerPessoa *instancia;             // Ponteiro para instância da classe.
+        ContainerPessoa(){};                           // Construtor.
+    public:
+       // Método para instanciar classe.
+        /**
+         * @brief implementa o padrão Singleton para garantir que apenas uma instância do container seja criada.
+         * @return Retorna um ponteiro para a instância única do container.
+         */
+        static ContainerPessoa* getInstancia();       
+       
+       // Métodos responsáveis por prestar serviços.
+        /**
+         * @brief Inclui uma nova pessoa no container.
+         * @param Pessoa é o objeto de domínio que representa a pessoa a ser incluída.
+         * @return Retorna verdadeiro se a inclusão for bem-sucedida, falso caso contrário
+         */
+        bool incluir(Pessoa);                          
+        /**
+         * @brief Remove uma pessoa do container com base no email fornecido.
+         * @param Email é a chave utilizada para identificar a pessoa a ser removida.
+         * @return Retorna verdadeiro se a remoção for bem-sucedida, falso caso contrário.
+         */
+        bool remover(Email);
+        /**
+         * @brief Pesquisa uma pessoa no container com base no email fornecido.
+         * @param Pessoa é um ponteiro para o objeto de domínio que será preenchido com os dados da pessoa encontrada.
+         * @return Retorna verdadeiro se a pesquisa for bem-sucedida e a pessoa for encontrada, falso caso contrário.
+         */
+        bool pesquisar(Pessoa*);
+        /**
+         * @brief Atualiza os dados de uma pessoa no container com base no objeto de domínio fornecido.
+         * @param Pessoa é o objeto de domínio que contém os novos dados da pessoa a serem atualizados.
+         * @return Retorna verdadeiro se a atualização for bem-sucedida, falso
+         */
+        bool atualizar(Pessoa);
+};
+
+#endif // CONTAINERPESSOA_H_INCLUDED
---
+++ source/Controladoras/CrtlServicoAutenticacao.cpp
@@ -0,0 +1,25 @@
+#include "Controladoras/CrtlServicoAutenticacao.hpp"
+
+/*
+CrtlServicoAutenticacao::CrtlServicoAutenticacao(ContainerPessoa* container) {
+    this->container = container;
+}
+*/
+
+bool CrtlServicoAutenticacao::autenticarPessoa(const Email& email, const Senha& senha) {
+    //pega os objetos de domínio Email e Senha e convertendo para string para poder fazer a autenticação
+    std::string emailStr = email.getValor();
+    std::string senhaStr = senha.getValor();
+
+    // 2. Chamamos o contêiner para verificar no banco de dados SQLite
+    // return  container->verificarCredenciais(emailStr, senhaStr);
+
+    // Implementação da lógica de autenticação
+    // Aqui você pode adicionar a lógica para verificar as credenciais
+    // usando o repositório ContainerPessoa.
+    // Por exemplo:
+    // return container->verificarCredenciais(email, senha);
+    
+    // Para fins de exemplo, vamos retornar true.
+    return true;
+}
