dbe6f5d62eb0a87e8941bd9a8d3a3ae40c56d49c
joaoF <jhonjhon2sj@gmail.com>
2026-06-23 14:16:08 -0300

    rename: nome do include stub autenticacao

---
+++ source/stubs/stubAutenticacao.cpp
@@ -0,0 +1,14 @@
+#include "Stubs/stubAutenticacao.hpp"
+
+const string StubServicoAutenticacao::EMAIL_VALIDO = "joaof@gmail.com";
+const string StubServicoAutenticacao::SENHA_VALIDA = "J1fa2b";
+
+bool StubServicoAutenticacao::autenticarPessoa(const Email& email, const Senha& senha){
+    if(email.getValor().compare(EMAIL_VALIDO) != 0)
+        return false;
+    if(senha.getValor().compare(SENHA_VALIDA) != 0)
+        return false;
+    return true;
+}
+
+
