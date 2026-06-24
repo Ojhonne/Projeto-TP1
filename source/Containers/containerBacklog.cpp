6cc5aa83659138fac6ed10527522524555f532ac
João Francisco <jhonjhon2sj@gmail.com>
2026-06-24 11:05:01 -0300

    feat: scheam historia de usuario concluido

--- source/Containers/containerBacklog.cpp
+++ source/Containers/containerBacklog.cpp
@@ -13,6 +13,39 @@ ContainerBacklog* ContainerBacklog::getInstancia(){
 }
 
 ContainerBacklog::ContainerBacklog(){
-    s
-}
+    sqlite3* db; 
+    int resultCode = sqlite3_open(nomeBanco.c_str(), &db); 
 
+    if (resultCode == SQLITE_OK) { 
+        std::string sql = "CREATE TABLE IF NOT EXISTS HistoriaDeUsuario (" 
+                            "codigo TEXT PRIMARY KEY, "
+                            "titulo TEXT NOT NULL, "
+                            "papel TEXT NOT NULL, "
+                            "acao TEXT NOT NULL, "
+                            "valor TEXT NOT NULL, "
+                            "estimativa INTEGER NOT NULL, "
+                            "prioridade TEXT NOT NULL, "
+                            "estado TEXT NOT NULL, "
+                            "projeto_codigo TEXT NOT NULL, "
+                            "sprint_codigo  TEXT, "
+                            "pessoa_email  TEXT, "
+                            "FOREIGN KEY(projeto_codigo) REFERENCES Projeto(codigo), "
+                            "FOREIGN KEY(sprint_codigo) REFERENCES PlanoDeSprint(codigo), "
+                            "FOREIGN KEY(pessoa_email) REFERENCES Pessoa(email));";
+                          
+        char* mensagemErro = nullptr;
+        resultCode = sqlite3_exec(db, sql.c_str(), nullptr, nullptr, &mensagemErro); 
+        
+        if (resultCode != SQLITE_OK) { 
+            std::string erro = mensagemErro;
+            std::cerr << "Erro ao criar tabela HistoriaDeUsuario: " << mensagemErro << std::endl;
+            sqlite3_free(mensagemErro);
+            sqlite3_close(db); 
+
+            throw std::runtime_error("Erro ao criar tabela HistoriaDeUsuario: " + erro);
+        
+        }
+    }
+    sqlite3_close(db); // Fecha o banco de dados SQLite.
+    
+}
