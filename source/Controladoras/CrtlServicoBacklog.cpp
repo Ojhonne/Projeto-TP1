a316f547efe3584041232d0a21886ffc18f64991
João Francisco <jhonjhon2sj@gmail.com>
2026-06-24 14:49:09 -0300

    feat: atualizar historias

--- source/Controladoras/CrtlServicoBacklog.cpp
+++ source/Controladoras/CrtlServicoBacklog.cpp
@@ -28,3 +28,17 @@ bool CrtlServicoBacklog::criarHistoriaUsuario(const HistoriaDeUsuario& historia)
         return false;
     }
  }
+
+  bool CrtlServicoBacklog::atualizarHistoriaUsuario(const HistoriaDeUsuario& historiaAtualizada){
+    try{
+        // Tenta ler uma historia  no banco de dados através do Singleton
+        if(ContainerBacklog::getInstancia()->atualizarHistoriaUsuario(historiaAtualizada)){
+            return true;
+        }
+        return false; // Não foi possivel criar a historia
+    } catch (const std::runtime_error& e){
+        std::cerr << "[Falha no MS-BACKLOG] Erro de persistência: " << e.what() << std::endl;
+        return false;
+    }
+ }
+ 
