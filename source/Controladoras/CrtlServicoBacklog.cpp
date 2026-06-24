54cc34448b410715239e674936fbb458dacc485c
João Francisco <jhonjhon2sj@gmail.com>
2026-06-24 14:47:50 -0300

    feat: lerr historias

--- source/Controladoras/CrtlServicoBacklog.cpp
+++ source/Controladoras/CrtlServicoBacklog.cpp
@@ -16,3 +16,15 @@ bool CrtlServicoBacklog::criarHistoriaUsuario(const HistoriaDeUsuario& historia)
         return false;
     }
  }
+ bool CrtlServicoBacklog::lerHistoriaUsuario(const Codigo& chaveID, HistoriaDeUsuario& armazenaHistoria ){
+    try{
+        // Tenta ler uma historia  no banco de dados através do Singleton
+        if(ContainerBacklog::getInstancia()->lerHistoriaUsuario(chaveID, armazenaHistoria)){
+            return true;
+        }
+        return false; // Não foi possivel criar a historia
+    } catch (const std::runtime_error& e){
+        std::cerr << "[Falha no MS-BACKLOG] Erro de persistência: " << e.what() << std::endl;
+        return false;
+    }
+ }
