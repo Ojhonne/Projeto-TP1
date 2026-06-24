e67e961cf5aee4edd40197f0c7ed49f02efdcc1a
João Francisco <jhonjhon2sj@gmail.com>
2026-06-24 14:44:12 -0300

    feat: criar historias

--- source/Controladoras/CrtlServicoBacklog.cpp
+++ source/Controladoras/CrtlServicoBacklog.cpp
@@ -3,3 +3,16 @@
 #include "Entidades/HistoriaDeUsuario.hpp"
 #include <stdexcept> 
 #include <iostream>
+
+bool CrtlServicoBacklog::criarHistoriaUsuario(const HistoriaDeUsuario& historia){
+    try{
+        // Tenta criar uma historia  no banco de dados através do Singleton
+        if(ContainerBacklog::getInstancia()->criarHistoriaUsuario(historia)){
+            return true;
+        }
+        return false; // Não foi possivel criar a historia
+    } catch (const std::runtime_error& e){
+        std::cerr << "[Falha no MS-BACKLOG] Erro de persistência: " << e.what() << std::endl;
+        return false;
+    }
+ }
